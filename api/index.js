70b93469fa50d0542cf764a2b13defb324be2fcd
Aditya Vishwanatha <aditya.vishwanatha@gmail.com>
2023-11-18 12:25:09 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+
+mongoose.connect("mongodb+srv://adi:adi@mernestate.iffjm3n.mongodb.net/mern-estate?retryWrites=true&w=majority").then(()=>{
+    console.log('Connected to Mongo')
+})
 
 const app = express();
 app.listen(3000,()=>{
